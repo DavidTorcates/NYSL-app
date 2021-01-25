fb53c61969b6e0b0f9fdd77e94a38cf528c4f64f
Roberto González <rob.gonzalez.castro@gmail.com>
2021-01-25 13:14:34 -0300

    B-icon problem, test login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,6 +11,10 @@
 import Card from "@/components/Card.vue";
 export default {
   name: "Home",
+  data(){
+    msg : 'shere'
+  }
+  ,
   components: {
     Card
   },
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -5,24 +5,28 @@
     </router-link>
     <div class="container2">
       <h3>Registro</h3>
-      <b-icon icon="envelope" class ="icon"></b-icon>
+      <b-icon icon="envelope" name="icon" class ="icon">
+        
+      </b-icon>
       <input class="input" 
       type="text" 
       placeholder="Email" 
       v-model="email" 
       maxlength="15"
       minlength="7"
-      pattern=".+@nysl.com"
       />
       <br />
       <b-icon icon="lock" class="icon"></b-icon>
       <input
         class="input"
-        type="password"
+        type="text"
         placeholder="Password"
         v-model="password"
       />
      <br/>
+     <div v-if="arrobaFunction" class="arrobaValidation">
+       El formato del email está correcto
+     </div>
       <button class="inputSignup" @click="signUp">registrar</button>
       <br />
       
@@ -44,13 +48,13 @@ export default {
   data() {
     return {
       email: "",
-      arrobaValue: this.arrobaFunction,
+      arrobaValue: true,
       password: "",
     };
   },
   computed: {
     arrobaFunction() {
-     return this.email.includes("user");
+     return this.email.includes("@") == true;
     }
   },
   methods: {
